c252c3d53b66ffa41fed3b54b7296eb52d9dc252
Ilya Kamenev <ilia.kamenevv@yandex.ru>
2021-09-22 19:13:33 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useEffect, useRef, useState} from "react";
+
+
+let startP={x:10,y:10};
+let endP={x:10,y:10};
+
+const App=(props)=>{
+    const canvasRef=useRef(null)
+    const contextRef=useRef(null)
+    const [isDrawing, setIsDrawing]=useState(false)
+    useEffect(()=>{
+        const canvas=canvasRef.current;
+        canvas.width=window.innerWidth*2;
+        canvas.height=window.innerHeight*2;
+        canvas.style.height=`${window.innerHeight}px`;
+        canvas.style.height=`${window.innerHeight}px`;
+        const context=canvas.getContext('2d');
+        context.scale(2,2);
+        context.lineCap='square';
+        context.strokeStyle='black'
+        context.lineWidth=3;
+        contextRef.current=context;
+    },[])
+
+
+    const startDrawing=({nativeEvent})=>{
+        const {offsetX,offsetY}=nativeEvent;
+        startP= {x:offsetX, y: offsetY};
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX,offsetY);
+        setIsDrawing(true)
+    }
+
+    const finishDrawing=({nativeEvent})=>{
+        const {offsetX,offsetY}=nativeEvent;
+        endP={x:offsetX,y:offsetY};
+        setIsDrawing(false);
+        contextRef.current.closePath();
+        drawRectangle();
+    }
+
+    const drawRectangle = () => {
+        contextRef.current.lineTo(startP.x, endP.y);
+        contextRef.current.lineTo(endP.x, endP.y);
+        contextRef.current.lineTo(endP.x, startP.y);
+        contextRef.current.lineTo(startP.x, startP.y);
+        contextRef.current.stroke();
+    }
+
+    return(
+        <div>
+            <canvas
+                onMouseDown={startDrawing}
+                onMouseUp={finishDrawing}
+                ref={canvasRef}
+            />
+        </div>
+    )
 }
 
 export default App;
